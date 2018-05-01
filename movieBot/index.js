d95ba9a568db28cd0ab908d37e9394cb5f74e35a
scye09 <sophiaye@wharton.upenn.edu>
2018-05-01 09:41:32 -0400

    Add files via upload

--- movieBot/index.js
+++ movieBot/index.js
@@ -12,9 +12,24 @@ const bot = new BootBot({
  * Demo handler to echo back whatever the user says.
  * Feel free to delete this handler and start hacking!
  */
-bot.on('message', (payload, chat) => {
-  const text = payload.message.text;
-  chat.say(`Echo: ${text}`);
+ // bot.setGetStartedButton((payload, chat) => {
+ //   const welcome1 = `Hey there, trainer! How well you think you ...`;
+ //   const welcome2 = `Type START or PLAY to join the challenge!`;
+ //   const options = { typing: true };
+ //   chat.say(welcome1, options)
+ //     .then(() => chat.say(welcome2, options));
+ // });
+
+
+ bot.on('message', (payload, chat) => {
+   console.log("sdfs");
+   const text = payload.message.text;
+   chat.say(`Echo: ${text}`);
+ });
+
+bot.hear(['hi', 'hello', 'hey'], (payload, chat) => {
+  chat.say('Hello, Type Genre then add the movie genre of the recommendation you want. ');
+  console.log("I'M WORKING!!");
 });
 
 bot.start(config.get('botPort'));
